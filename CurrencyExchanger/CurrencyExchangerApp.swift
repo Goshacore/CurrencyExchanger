0cefdf0e774b50d05de395a4e70198f62ec26852
Goshacore <75911229+Goshacore@users.noreply.github.com>
2022-10-31 17:03:14 +0300

    Initial commit

---
+++ CurrencyExchanger/CurrencyExchangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyExchangerApp.swift
+//  CurrencyExchanger
+//
+//  Created by Heorhi on 2022-10-31.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyExchangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
